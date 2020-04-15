7021c8665505de8d29c4223ff15ab232e3f9db22
lgactna <48928674+lgactna@users.noreply.github.com>
2020-04-14 23:05:25 -0700

    add basic plotting functionality

--- scripts/data.js
+++ scripts/data.js
@@ -2,4 +2,9 @@ let TESTER = document.getElementById('tester');
 Plotly.newPlot( TESTER, [{
     x: [1, 2, 3, 4, 5],
     y: [1, 2, 4, 8, 16] }], {
-margin: { t: 0 } } );
+margin: { t: 0, r: 0, b: 25, l: 25 },
+plot_bgcolor:"#111", 
+paper_bgcolor:"#111", 
+font: {family:"Exo 2", color:"#FFF"}}
+ );
+//https://plotly.com/javascript/reference/#layout-paper_bgcolor
